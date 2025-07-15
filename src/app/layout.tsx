a5e71fb35479132869adf1caf0a97e7f3d1e3eb1
Salman Khan <khansolo23@gmail.com>
2025-07-15 15:01:48 +0000

    Add g-tag:
    <!-- Google tag (gtag.js) -->
    <script async src="https://www

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@
  * and wraps all page content. It also provides a `Toaster` component for notifications.
  */
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
@@ -25,6 +26,16 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&family=Inter:wght@400;600&family=Space+Grotesk:wght@400;700&family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DMPEHZXTC8"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DMPEHZXTC8');
+          `}
+        </Script>
         <div className="flex-grow">{children}</div>
         <Toaster />
       </body>
