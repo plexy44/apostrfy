ddab6faec3a3471b85116090b9632cb2e87000a3
Salman Khan <khansolo23@gmail.com>
2025-12-10 22:39:57 +0000

    Try fixing this error: `Console Error: GenkitError: NOT_FOUND: Model 'ge

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta'})],
+  plugins: [googleAI()],
   model: 'gemini-1.5-flash-latest',
 });
